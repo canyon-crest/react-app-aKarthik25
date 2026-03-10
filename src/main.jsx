c47f804e90e94fd7e36e072b2f308bc3f7bd3659
aKarthik25 <karthika2870@my.sduhsd.net>
2026-03-10 13:30:04 -0700

    Updated pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Nav from './Nav.jsx'
+import Nav from './components/Nav.jsx'
 import Card from './Card.jsx'
 import Footer from './Footer.jsx'
 createRoot(document.getElementById('root')).render(
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+
+function Home () {
+    return (
+        <h1> Are you interested in learning about 3D modeling?</h1>
+    )
+}
+export default Home;
